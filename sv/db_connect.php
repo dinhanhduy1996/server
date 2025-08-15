06647599c876a917ff6189fb66562d7ef431354d
duy <bachtuhoa20@gmail.com>
2025-08-16 02:01:53 +0700

    a

--- sv/db_connect.php
+++ sv/db_connect.php
@@ -1,23 +1,23 @@
 <?php
-// Thông tin kết nối database
-$servername = "127.0.0.1";
-$username = getenv("db_user");
-$password = getenv("db_pass");
-$dbname   = getenv("db_name");
+// Thông tin kết nối database Aiven
+$servername = "mysql-a2448e4-bachtuhoa20-0662.h.aivencloud.com";
+$port       = 20704;
+$username   = "avnadmin";
+$password   = "AVNS_Vhe33AA3inpWDMgr1qO";
+$dbname     = "defaultdb";
 
-// Tạo kết nối bằng MySQLi
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
-if ($conn->connect_error) {
+// Khởi tạo kết nối MySQLi với SSL
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
     http_response_code(500); // Internal Server Error
-    echo json_encode(array("status" => "error", "message" => "Lỗi kết nối database: " . $conn->connect_error));
+    echo json_encode(array("status" => "error", "message" => "Lỗi kết nối database: " . mysqli_connect_error()));
     die();
 }
 
 // Thiết lập charset để hỗ trợ tiếng Việt
-$conn->set_charset("utf8mb4");
+mysqli_set_charset($conn, "utf8mb4");
 
 // Thiết lập múi giờ MySQL về giờ Việt Nam
-$conn->query("SET time_zone = '+07:00'");
+mysqli_query($conn, "SET time_zone = '+07:00'");
 ?>
