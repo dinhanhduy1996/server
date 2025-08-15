9830a768271b1a8a6870e3dd7da27b4721e74957
duy <bachtuhoa20@gmail.com>
2025-08-16 02:07:35 +0700

    a

--- sv/db_connect.php
+++ sv/db_connect.php
@@ -1,22 +1,17 @@
 <?php
-// Thông tin kết nối database từ biến môi trường
-$servername = getenv("DB_HOST");
-$port       = getenv("DB_PORT");
-$username   = getenv("DB_USER");
-$password   = getenv("DB_PASS");
-$dbname     = getenv("DB_NAME");
+// Thông tin kết nối database Aiven
+$servername = "mysql-a2448e4-bachtuhoa20-0662.h.aivencloud.com";
+$port       = 20704;
+$username   = "avnadmin";
+$password   = "AVNS_Vhe33AA3inpWDMgr1qO";
+$dbname     = "defaultdb";
 
 // Khởi tạo kết nối MySQLi với SSL
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-
-// Kết nối tới MySQL (Aiven yêu cầu SSL)
-if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, (int)$port, NULL, MYSQLI_CLIENT_SSL)) {
+if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
     http_response_code(500); // Internal Server Error
-    echo json_encode(array(
-        "status" => "error",
-        "message" => "Lỗi kết nối database: " . mysqli_connect_error()
-    ));
+    echo json_encode(array("status" => "error", "message" => "Lỗi kết nối database: " . mysqli_connect_error()));
     die();
 }
 
